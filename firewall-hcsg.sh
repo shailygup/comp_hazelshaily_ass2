55e26cc652931c32f1dd4bb32495e62c476f2bca
Shaily Gupta <shaily.gupta@cloudreach.com>
2018-02-06 13:17:41 -0800

    Adding some more rules, might need to recheck this again

--- firewall-hcsg.sh
+++ firewall-hcsg.sh
@@ -10,7 +10,7 @@ NET=  #the network device
 TCPALLOW= #TCP services that will be allowed.
 UDPALLOW= #UDP services that will be allowed
 ICMPALLOW=  #ICMP services that will be allowed.
-PORTFROP= #Ports to be dropped
+PORTDROP= #Ports to be dropped
 
 #------------------------------
 #Implementation section
@@ -39,6 +39,8 @@ iptables -N icmpout #Might not be the correct way, since we need to do it via th
 #Drop all packets where the SYN and FIN bits are set
 
 #DROP ALL TELNET PACKETS - Not allowed at all 
+iptables -A tcpin -p tcp -m multiport --dport $PORTDROP -j DROP
+iptables -A tcpout -p tcp -m multiport --sport $PORTDROP -j DROP
 
 #Block all external traffic to 32768 – 32775, 137 – 139, TCP ports 111 and 515. 
 
@@ -53,9 +55,9 @@ iptables -A udpin -p tcp -m multiport --dport $UDPALLOW -j ACCEPT
 iptables -A icmpin -p tcp -m multiport --dport $ICMPALLOW -j ACCEPT
 
 #Allow all outbound traffic
-iptables -A tcpout -p tcp -m multiport --dport $TCPALLOW -j ACCEPT
-iptables -A udpout -p tcp -m multiport --dport $UDPALLOW -j ACCEPT
-iptables -A icmpout -p tcp -m multiport --dport $ICMPALLOW -j ACCEPT
+iptables -A tcpout -p tcp -m multiport --sport $TCPALLOW -j ACCEPT
+iptables -A udpout -p tcp -m multiport --sport $UDPALLOW -j ACCEPT
+iptables -A icmpout -p tcp -m multiport --sport $ICMPALLOW -j ACCEPT
 
 #Correlating tcpin/tcpout/udpin/udpout with the INPUT/OUTPUT rules
 iptables -A INPUT -p tcp -j tcpin
