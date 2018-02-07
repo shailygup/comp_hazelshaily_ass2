bc5f9ab960ec9333eb62cf3e2006bacf8e80cdc1
Shaily Gupta <shaily.gupta@cloudreach.com>
2018-02-07 14:12:26 -0800

    Added some more rules

--- firewall-hcsg.sh
+++ firewall-hcsg.sh
@@ -29,12 +29,18 @@ iptables -N tcpin
 iptables -N tcpout
 iptables -N udpin
 iptables -N udpout
-iptables -N icmpin #Might not be the correct way, since we need to do it via the type
-iptables -N icmpout #Might not be the correct way, since we need to do it via the type
+iptables -N icmpin 
+iptables -N icmpout 
+
+# Allow inbound traffic from established connections.
+# This includes ICMP error returns.
+iptables A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 #Do not accept any packets with a source address from the outside matching your internal network
+iptables -A FORWARD -s !192.168.0.0/24 -i $LOCATION -j DROP #NOT SURE IF THIS IS CORRECT, PLEASE CHECK
 
 #Drop traffic that are coming in from high ports (inbound SYN patchets to high ports)
+iptables 
 
 #Drop all packets where the SYN and FIN bits are set
 
@@ -50,19 +56,22 @@ iptables -A tcpout -p tcp -m multiport --sport $PORTDROP -j DROP
 #Set Special "Minimum Delay" for FTP and SSH services
 
 #Allow all inbound traffic, REMEMBER TO ALLOW ESTABLISHED CONNECTION -- Still needs to be done
-iptables -A tcpin -p tcp -m multiport --dport $TCPALLOW -j ACCEPT
-iptables -A udpin -p tcp -m multiport --dport $UDPALLOW -j ACCEPT
-iptables -A icmpin -p tcp -m multiport --dport $ICMPALLOW -j ACCEPT
+iptables -A tcpin -i $LOCATION -p tcp -m multiport --dport $TCPALLOW -m state --state NEW -j ACCEPT 
+iptables -A udpin -i $LOCATION -p tcp -m multiport --dport $UDPALLOW -m state --state NEW -j ACCEPT
+
+# Allow ping.
+iptables -A icmpin -p icmp -m state --state NEW --icmp-type $ICMPALLOW -j ACCEPT
+
 
 #Allow all outbound traffic
-iptables -A tcpout -p tcp -m multiport --sport $TCPALLOW -j ACCEPT
-iptables -A udpout -p tcp -m multiport --sport $UDPALLOW -j ACCEPT
-iptables -A icmpout -p tcp -m multiport --sport $ICMPALLOW -j ACCEPT
+iptables -A tcpout -i $LOCATION -p tcp -m multiport --sport $TCPALLOW -m state --state NEW -j ACCEPT
+iptables -A udpout -i $LOCATION -p tcp -m multiport --sport $UDPALLOW -m state --state NEW -j ACCEPT
+iptables -A icmpout -i $LOCATION -p icmp -m state --icmp-type $ICMPALLOW -j ACCEPT
 
 #Correlating tcpin/tcpout/udpin/udpout with the INPUT/OUTPUT rules
-iptables -A INPUT -p tcp -j tcpin
+iptables -A FORWARD -p tcp -j tcpin
 iptables -A OUTPUT -p tcp -j tcpout
-iptables -A INPUT -p udp -j udpin
+iptables -A FORWARD -p udp -j udpin
 iptables -A OUTPUT -p udp -j udpout
-iptables -A INPUT -p icmp -j icmpin #Might not be the correct way, since we need to do it via the type
-iptables -A OUTPUT -p icmp -j icmpout #Might not be the correct way, since we need to do it via the type
+iptables -A FORWARD -p icmp -j icmpin 
+iptables -A OUTPUT -p icmp -j icmpout 
